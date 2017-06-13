011423e6ef678028cb46bdc7f5412ffbddc1ea00
ross carter <ross.carter@jic.ac.uk>
2017-06-13 14:28:43 +0100

    FINALLY fixed extract cell data to calculate distances correctly

--- scripts/extract_celldata.py
+++ scripts/extract_celldata.py
@@ -51,6 +51,28 @@ def do_kde(size, voxel, data_dict):
     return np.fliplr(relative_density)
 
 
+def return_coefficient(t_x, t_y, b_x, b_y):
+    # equation of line though tip and base
+    m_mv = (t_y - b_y) / (t_x - b_x)
+    c_mv = t_y - m_mv * t_x
+
+    # equation of line though tip, perpendicular to tip-base line
+    m_tip = -1 / m_mv
+    c_tip = t_y - m_tip * t_x
+
+    a = m_tip
+    b = -1
+    c = c_tip
+    return a, b, c
+
+
+def return_distances(a, b, c, t_x, t_y, b_x, b_y, x_c, y_c):
+    distance_mv_pix = np.abs((b_y - t_y) * x_c - (b_x - t_x) * y_c + b_x * t_y - b_y * t_x) / np.sqrt(
+        (b_y - t_y) ** 2 + (b_x - t_x) ** 2)
+    distance_tip_pix = np.abs(a * x_c + b * y_c + c) / np.sqrt(a ** 2 + b ** 2)
+    return distance_mv_pix, distance_tip_pix
+
+
 def main():
     """main function extracts and writes data"""
     exp_dir = args.exp_dir
@@ -75,29 +97,12 @@ def main():
     (t_x, t_y) = (float(tip[1]), float(size[0] - tip[0]))
     (b_x, b_y) = (float(base[1]), float(size[0] - base[0]))
 
-    # equation of line though tip and base
-    m_mv = (t_y - b_y) / (t_x - b_x)
-    c_mv = t_y - m_mv * t_x
+    (a, b, c) = return_coefficient(t_x, t_y, b_x, b_y)
 
-    # equation of line though tip, perpendicular to tip-base line
-    m_tip = -1 / m_mv
-    c_tip = t_y - m_tip * t_x
-
-    a = m_tip
-    b = -1
-    c = c_tip
-
-    # as above but in real units
-    (b_x_r, b_y_r) = (float(b_x) * voxel[1], float(b_y) * voxel[0])
     (t_x_r, t_y_r) = (float(t_x) * voxel[1], float(t_y) * voxel[0])
+    (b_x_r, b_y_r) = (float(b_x) * voxel[1], float(b_y) * voxel[0])
 
-    m_mv_r = float(t_y_r - b_y_r) / float(t_x_r - b_x_r)
-    m_tip_r = -1 / m_mv_r
-
-    c_tip_r = t_y_r - m_tip_r * t_x_r
-    a_r = m_tip_r
-    b_r = -1
-    c_r = c_tip_r
+    a_r, b_r, c_r = return_coefficient(t_x_r, t_y_r, b_x_r, b_y_r)
 
     # calculate cell level data
 
@@ -113,14 +118,10 @@ def main():
         centroid_y_pixels = cell['centroid'][0]
 
         (x_c, y_c) = (centroid_x_pixels, size[0] - centroid_y_pixels)
-        distance_from_mv_pix = np.abs((b_y - t_y) * x_c - (b_x - t_x) * y_c + b_x * t_y - b_y * t_x) / np.sqrt(
-            (b_y - t_y) ** 2 + (b_x - t_x) ** 2)
-        distance_from_tip_pix = np.abs(a * x_c + b * y_c + c) / np.sqrt(a ** 2 + b ** 2)
+        distance_mv_pix, distance_tip_pix = return_distances(a, b, c, t_x, t_y, b_x, b_y, x_c, y_c)
 
-        (x_c_r, y_c_r) = (float(x_c) * voxel[1], float(t_y) * voxel[0])
-        distance_from_mv_real = np.abs((b_y_r - t_y_r) * x_c_r - (b_x_r - t_x_r) * y_c_r + b_x_r * t_y_r -
-                                       b_y_r * t_x_r) / np.sqrt((b_y_r - t_y_r) ** 2 + (b_x_r - t_x_r) ** 2)
-        distance_from_tip_real = np.abs(a_r * x_c_r + b_r * y_c_r + c_r) / np.sqrt(a_r ** 2 + b_r ** 2)
+        (x_c_r, y_c_r) = (centroid_x_pixels * voxel[1], (size[0] - centroid_y_pixels) * voxel[0])
+        distance_mv_real, distance_tip_real = return_distances(a_r, b_r, c_r, t_x_r, t_y_r, b_x_r, b_y_r, x_c_r, y_c_r)
 
         centroid_x_real = centroid_x_pixels * voxel[1]
         centroid_y_real = centroid_y_pixels * voxel[0]
@@ -135,10 +136,10 @@ def main():
                      'Centroid-x_um': float(centroid_x_real),
                      'Centroid-y_um': float(centroid_y_real),
                      'Perimeter_um': float(perimeter),
-                     'Distance-from-mv_pixels': np.abs(distance_from_mv_pix),
-                     'Distance-from-tip_pixels': np.abs(distance_from_tip_pix),
-                     'Distance-from-mv_um': np.abs(distance_from_mv_real),
-                     'Distance-from-tip_um': np.abs(distance_from_tip_real),
+                     'Distance-from-mv_pixels': np.abs(distance_mv_pix),
+                     'Distance-from-tip_pixels': np.abs(distance_tip_pix),
+                     'Distance-from-mv_um': np.abs(distance_mv_real),
+                     'Distance-from-tip_um': np.abs(distance_tip_real),
                      'Circularity_none': circularity}
 
         # TODO ADD MORE STUFF HERE IF NEEDED
